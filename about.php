90994226c7a9acf344aea6e21c950ff2b5e2c537
helloaryan <68951337+helloaryan@users.noreply.github.com>
2020-07-29 16:48:02 +0530

    upload file

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include "header.php" ?>
+
+<div class="container my-5">	
+<div class="row featurette d-flex justify-content-center align-item-center">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">We Eastablished in 1745. <span class="text-muted">We are experianced.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+      	<img src="images/about1.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="400">
+      </div>
+    </div>.
+    </div>
+
+
+    <div class="container">
+    	<div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="images/about1.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="400">
+      </div>
+    </div>
+    </div>
+<?php include "footer.php" ?>
+ 
---
+++ index.php
@@ -0,0 +1,104 @@
+<?php include "header.php" ?>
+<!-- slider start -->
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/c4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      	<button  class="btn btn-primary">Join us</button>
+      	<button  class="btn btn-success">New Member</button>
+      	<button  class="btn btn-danger">Any Query</button>
+      	<h5 class="text-danger">WELCOME TO MY_BANK</h5>
+        <p>Get More Interest On Your Money</p>
+
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/c5.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <button  class="btn btn-primary">Join us</button>
+      	<button  class="btn btn-success">New Member</button>
+      	<button  class="btn btn-danger">Any Query</button>
+      	<h5 class="text-danger">WELCOME TO MY_BANK</h5>
+        <p>Get More Interest On Your Money</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/c6.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <button  class="btn btn-primary">Join us</button>
+      	<button  class="btn btn-success">New Member</button>
+      	<button  class="btn btn-danger">Any Query</button>
+      	<h5 class="text-danger">WELCOME TO MY_BANK</h5>
+        <p>Get More Interest On Your Money</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- navbar end -->
+
+<div class="container-fluid my-5">
+  <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+    <div class="col-md-6 px-0">
+      <h1 class="display-2 font-italic">WHY JOIN US</h1>
+      <p class="lead my-3">A bank is a financial institution that accepts deposits from the public and creates a demand deposit, while simultaneously making loans. Lending activities can ...</p>
+      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+	<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Our Features</h1>
+    <p class="lead">Some of the features are listed Below</p>
+  </div>
+</div>
+
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">World</strong>
+          <h3 class="mb-0">Featured post</h3>
+          <div class="mb-1 text-muted">Nov 12</div>
+          <p class="card-text mb-auto">This is a wider card with sup.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+        <img src="images/c-1.jpg" class="bd-placeholder-img" width="200" height="250">
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Design</strong>
+          <h3 class="mb-0">Post title</h3>
+          <div class="mb-1 text-muted">Nov 11</div>
+          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img src="images/c-1.jpg" class="bd-placeholder-img" width="200" height="250">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>	
+<?php include
+"footer.php" ?>
